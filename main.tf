a74c748fc53c0f4bc708bb987d28ee56077eb21a
Logeshkumarasamy1998 <logesh.kumarasamy@anywhere.co>
2024-12-14 00:11:45 +0530

    removed git action

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "logesh-all-test"
+  region  = "us-central1"
+  zone    = "us-central1-f"
+}
+
+resource "google_compute_instance" "example" {
+  name         = "fincalc"
+  machine_type = "e2-small"
+  zone         = "us-central1-f"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20241115"
+      size  = 10
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network    = "default"
+    subnetwork = "default"
+
+    access_config {
+      nat_ip       = "34.55.209.160" # Optional, can be omitted for auto-assigned IP
+      network_tier = "PREMIUM"
+    }
+  }
+
+  service_account {
+    email  = "462199367704-compute@developer.gserviceaccount.com"
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+
+  tags = ["http-server", "https-server"]
+
+  shielded_instance_config {
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+  }
+}
